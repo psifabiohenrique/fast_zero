81ef32c25165f2f670109adaadb8c4201778499d
Fábio Henrique <psifabiohenrique@outlook.com>
2024-06-14 17:27:09 -0300

    Atualizações após a aula 2 escrita

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,29 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def html_response():
+    return """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá mundos</title>
+</head>
+<body>
+    <h1>Olá Mundo!</h1>
+    <p> Estamos aprendendo a fazer APIs com FastAPI!</p>
+</body>
+</html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,26 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_html_response_deve_retornar_ok_e_html():
+    client = TestClient(app)
+
+    response = client.get('/html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.text
+        == """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá mundos</title>
+</head>
+<body>
+    <h1>Olá Mundo!</h1>
+    <p> Estamos aprendendo a fazer APIs com FastAPI!</p>
+</body>
+</html>"""
+    )
